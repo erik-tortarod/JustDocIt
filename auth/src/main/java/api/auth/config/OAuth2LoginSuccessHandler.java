e2bf5b02b76aad0891d5ecf3ed766fdbea1430f7
NewName4Me <tortarod@gmail.com>
2025-03-26 19:30:46 +0100

    SSD-2 | feat: store Oauth2 users in mongodb

---
+++ auth/src/main/java/api/auth/config/OAuth2LoginSuccessHandler.java
@@ -0,0 +1,57 @@
+package api.auth.config;
+
+import api.auth.service.UserService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private OAuth2AuthorizedClientService clientService;
+
+    @Override
+    public void onAuthenticationSuccess(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Authentication authentication
+    ) throws IOException, ServletException {
+
+        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+        OAuth2User oAuth2User = oauthToken.getPrincipal();
+
+        //Get GitHub Access Token
+        OAuth2AuthorizedClient client = clientService.loadAuthorizedClient(
+                oauthToken.getAuthorizedClientRegistrationId(),
+                oauthToken.getName()
+        );
+
+        String accessToken = client.getAccessToken().getTokenValue();
+
+        //Save user in mongodb
+        userService.processOAuthUser(
+                oauthToken.getAuthorizedClientRegistrationId(),
+                oAuth2User.getAttributes(),
+                accessToken
+        );
+
+        //Redirect the user
+        getRedirectStrategy().sendRedirect(request,response,"/dashboard");
+
+    }
+
+}

3e4904e1d07196c989ad6326383864c60e1c2f69
NewName4Me <tortarod@gmail.com>
2025-03-26 22:50:06 +0100

    SSD-13 | fix: removing unnecessary token
    GitHub, contrary to other Oauth2 providers, does not provide a refresh token, therefore i don't need to store an undefined attribute

--- auth/src/main/java/api/auth/service/UserService.java
+++ auth/src/main/java/api/auth/service/UserService.java
@@ -20,7 +20,7 @@ public class UserService {
             return processGithubUser(attributes, accessToken);
         }
 
-        throw  new UnsupportedOperationException("Provider not supported "+ provider);
+        throw new UnsupportedOperationException("Provider not supported "+ provider);
     }
 
     private User processGithubUser(Map<String, Object> attributes, String accessToken){

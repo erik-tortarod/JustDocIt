285c35d8160391b7d887247530c5faa74ae4a766
NewName4Me <tortarod@gmail.com>
2025-03-28 20:56:46 +0100

    CSD-7 | Implementing basic routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
+//DEPENDENCIES
+import { Route, Routes } from "react-router-dom";
+
+//PAGES
+import Auth from "./pages/Auth/Auth";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import NotFound from "./pages/NotFound/NotFound";
+
 function App() {
-	return (
-		<div>
-			<p>dawd</p>
-			<h2>dwada</h2>
-			<h6>dawda</h6>
-		</div>
-	);
+   return (
+      <Routes>
+         <Route path="/" element={<Auth />} />
+         <Route path="/dashboard" element={<Dashboard />} />
+         <Route path="/notfound" element={<NotFound />} />
+      </Routes>
+   );
 }
 
 export default App;

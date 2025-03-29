c646cf88faf7c35987a77ce4df7fb82906a9c7f3
NewName4Me <tortarod@gmail.com>
2025-03-29 11:56:22 +0100

    CSD-7 | feat(fronted): adding localStorage functionality to keep the user in after recharging the page

--- frontend/src/services/AuthService.ts
+++ frontend/src/services/AuthService.ts
@@ -3,45 +3,53 @@ import ApiService from "./ApiService";
 import StorageService from "./StorageService";
 
 class AuthService {
-	static async processUrlParams(): Promise<{
-		sucess: boolean;
-		error?: string;
-	}> {
-		try {
-			//get url params
-			const params = new URLSearchParams(window.location.search);
-			const userId = params.get("userId");
-			const accessToken = params.get("accessToken");
+   static async processUrlParams(): Promise<{
+      sucess: boolean;
+      error?: string;
+   }> {
+      try {
+         //get url params
+         const params = new URLSearchParams(window.location.search);
+         const userId = params.get("userId");
+         const accessToken = params.get("accessToken");
 
-			if (!userId || !accessToken) {
-				return {
-					sucess: false,
-					error: `Some params are missing`,
-				};
-			}
+         if (userId && accessToken) {
+            const jwtToken = await ApiService.getTokenFromCredentials(userId, accessToken);
 
-			const jwtToken = await ApiService.getTokenFromCredentials(
-				userId,
-				accessToken,
-			);
+            StorageService.setToken(jwtToken);
 
-			StorageService.setToken(jwtToken);
+            window.history.replaceState({}, document.title, "/dashboard");
 
-			window.history.replaceState({}, document.title, "/dashboard");
+            return { sucess: true };
+         }
 
-			return { sucess: true };
-		} catch (error) {
-			return {
-				sucess: false,
-				error: `Error: ${String(error)}`,
-			};
-		}
-	}
+         //no url params
+         const savedToken = StorageService.getToken();
+         if (savedToken) {
+            return { sucess: true };
+         }
 
-	static logout(): void {
-		StorageService.removeToken();
-		window.location.href = "/";
-	}
+         return {
+            sucess: false,
+            error: `No authentication info available`
+         };
+
+      } catch (error) {
+         return {
+            sucess: false,
+            error: `Error: ${String(error)}`,
+         };
+      }
+   }
+
+   static isAuthenticated(): boolean {
+      return StorageService.getToken() !== null;
+   }
+
+   static logout(): void {
+      StorageService.removeToken();
+      window.location.href = "/";
+   }
 }
 
 export default AuthService;

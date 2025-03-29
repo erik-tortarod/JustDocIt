b63295298412862e324d4684300603f728a33f63
NewName4Me <tortarod@gmail.com>
2025-03-29 11:28:49 +0100

    CSD-7 | creating a service in charge of managing the client localStorage

---
+++ frontend/src/services/StorageService.ts
@@ -0,0 +1,34 @@
+const STORAGE_KEYS = {
+   JWT_TOKEN: 'jwtToken',
+   USER_PREFERENCES: 'userPreferences'
+}
+
+class StorageService {
+
+   //JWT Token
+   static getToken = (): string | null => {
+      return localStorage.getItem(STORAGE_KEYS.JWT_TOKEN);
+   }
+   static setToken = (token: string): void => {
+      localStorage.setItem(STORAGE_KEYS.JWT_TOKEN, token);
+   }
+   static removeToken = (): void => {
+      localStorage.removeItem(STORAGE_KEYS.JWT_TOKEN);
+   }
+
+   //User Preferences
+   static saveUserPreferences = (preferences: any): void => {
+      localStorage.setItem(STORAGE_KEYS.USER_PREFERENCES, JSON.stringify(preferences));
+   }
+   static getUserPreferences = (): any | null => {
+      const preferences = localStorage.getItem(STORAGE_KEYS.USER_PREFERENCES);
+      return preferences ? JSON.parse(preferences) : null;
+   }
+
+   //Clear All
+   static clearAll = (): void => {
+      localStorage.clear();
+   }
+}
+
+export default StorageService;

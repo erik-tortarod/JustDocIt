3ec47737368fdc750ad274f98758f9afa8d242c2
NewName4Me <tortarod@gmail.com>
2025-03-30 18:02:37 +0200

    CSD-9 | displaying the actual repositories in a simple way

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -5,61 +5,95 @@ import { useState, useEffect } from "react";
 import ApiService from "../../services/ApiService";
 import AuthService from "../../services/AuthService";
 import StorageService from "../../services/StorageService";
+import RepositoryService from "../../services/RepositoryService";
 
 function Dashboard() {
-	const [loading, setLoading] = useState<boolean>(true);
-	const [error, setError] = useState<string | undefined>(undefined);
-	const [userData, setUserData] = useState<any>(null);
+   const [loading, setLoading] = useState<boolean>(true);
+   const [error, setError] = useState<string | undefined>(undefined);
+   const [userData, setUserData] = useState<any>(null);
+   const [userRepositories, setUserRepositores] = useState<any>();
 
-	useEffect(() => {
-		const initializeDashboard = async () => {
-			try {
-				if (!StorageService.getToken() && !window.location.search) {
-					setError("You have not logged in yet");
-					setLoading(false);
-					return;
-				}
+   useEffect(() => {
+      const initializeDashboard = async () => {
+         try {
+            if (!StorageService.getToken() && !window.location.search) {
+               setError("You have not logged in yet");
+               setLoading(false);
+               return;
+            }
 
-				const authResult = await AuthService.processUrlParams();
+            const authResult = await AuthService.processUrlParams();
 
-				if (!authResult.sucess) {
-					setError(authResult.error);
-					setLoading(false);
-					return;
-				}
+            if (!authResult.sucess) {
+               setError(authResult.error);
+               setLoading(false);
+               return;
+            }
 
-				const userData = await ApiService.getUserData();
-				setUserData(userData);
-			} catch (error) {
-				console.error(`Error: ${error}`);
-				setError(`Error : ${error}`);
-			} finally {
-				setLoading(false);
-			}
-		};
+            const userData = await ApiService.getUserData();
+            setUserData(userData);
+         } catch (error) {
+            console.error(`Error: ${error}`);
+            setError(`Error : ${error}`);
+         } finally {
+            setLoading(false);
+         }
+      };
 
-		initializeDashboard();
-	}, []);
+      const getUserRepositories = async () => {
+         const repositories = await RepositoryService.getUserRepositories();
+         setUserRepositores(repositories);
+      };
 
-	if (loading) {
-		return <div>Cargando ...</div>;
-	}
+      initializeDashboard().then(() => getUserRepositories());
+   }, []);
 
-	if (error) {
-		return (
-			<div>
-				<h1>Error</h1>
-				<button>Volver al login</button>
-			</div>
-		);
-	}
+   if (loading) {
+      return <div>Cargando ...</div>;
+   }
 
-	return (
-		<div>
-			<h1>Dashboard</h1>
-			<pre>{JSON.stringify(userData, null, 2)}</pre>
-		</div>
-	);
+   if (error) {
+      return (
+         <div>
+            <h1>Error</h1>
+            <button>Volver al login</button>
+         </div>
+      );
+   }
+
+   return (
+      <div>
+         <h1>Dashboard</h1>
+         <section>
+            <h2>Datos del Usuario</h2>
+            <ul>
+               <li>
+                  <img src={userData.avatarUrl} alt="" />
+               </li>
+               <li>
+                  <strong>Email:</strong> {userData?.email}
+               </li>
+               <li>
+                  <strong>Username:</strong> {userData?.username}
+               </li>
+            </ul>
+         </section>
+         <section>
+            <h2>Repositorios</h2>
+            <ul>
+               {userRepositories?.map((repo: any) => (
+                  <li key={repo.id}>
+                     <p>
+                        <strong>{repo.name}</strong> -{" "}
+                        {repo.description || "Sin descripción"}
+                        {repo.private && "🔒"}
+                     </p>
+                  </li>
+               ))}
+            </ul>
+         </section>
+      </div>
+   );
 }
 
 export default Dashboard;

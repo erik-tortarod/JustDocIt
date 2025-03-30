033d971c85ebc540c710bcb8c2378a76d547f622
NewName4Me <tortarod@gmail.com>
2025-03-30 18:02:37 +0200

    CSD-9 | displaying the actual repositories in a simple way

--- frontend/src/config/api-routes.ts
+++ frontend/src/config/api-routes.ts
@@ -1,4 +1,5 @@
 const API_BASE_URL = import.meta.env.VITE_API_URL;
+const DOCS_BASE_URL = import.meta.env.VITE_DOCS_URL;
 
 export const API_ROUTES = Object.freeze({
 	AUTH: {
@@ -6,4 +7,7 @@ export const API_ROUTES = Object.freeze({
 		USER: `${API_BASE_URL}/api/user`,
 		TOKEN: `${API_BASE_URL}/auth/token`,
 	},
+	DOCS: {
+		REPOSITORIES: `${DOCS_BASE_URL}/api/github-repositories`
+	}
 });

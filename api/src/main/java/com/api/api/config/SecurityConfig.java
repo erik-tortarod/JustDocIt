b8e83ab1a077978340fd590773c701e83038bc09
NewName4Me <tortarod@gmail.com>
2025-03-30 12:25:51 +0200

    CICD-50 | fix entrypoint problem
    in the docker-compose the entrypoint was set as 8080, but in
    the spring boot app, its seet as 8082, therefore its not getting it properly

--- api/src/main/java/com/api/api/config/SecurityConfig.java
+++ api/src/main/java/com/api/api/config/SecurityConfig.java
@@ -2,9 +2,13 @@ package com.api.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -13,10 +17,19 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Disable CSRF for simplicity
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration configuration = new CorsConfiguration();
+                    configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+                    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                    configuration.setAllowedHeaders(Arrays.asList("*"));
+                    configuration.setAllowCredentials(true);
+                    return configuration;
+                }))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/test", "/api/decode-jwt", "/api/decrypt-access-token","/api/github-repositories").permitAll() // Allow unauthenticated access to specific endpoints
-                        .anyRequest().authenticated() // Require authentication for other endpoints
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Explicitly allow OPTIONS
+                        .requestMatchers("/", "/error", "/css/**", "/js/**", "/api/**", "/auth/token", "/token/**").permitAll()
+                        .anyRequest().authenticated()
                 );
         return http.build();
     }

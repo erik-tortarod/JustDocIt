0f74b94637fcf71b68b8f0de5fdd233f3a799958
NewName4Me <tortarod@gmail.com>
2025-03-30 18:02:19 +0200

    CSD-9 | creating specfic model and services for easiar manipulaation of the repositories

---
+++ frontend/src/services/RepositoryService.ts
@@ -0,0 +1,28 @@
+import { API_ROUTES } from "../config/api-routes";
+import StorageService from "./StorageService";
+
+class RepositoryService {
+
+   static async getUserRepositories(): Promise<any> {
+      const token = StorageService.getToken();
+
+      if (!token) {
+         throw new Error(`No authentication token`);
+      }
+
+      const response = await fetch(API_ROUTES.DOCS.REPOSITORIES, {
+         headers: {
+            Authorization: `Bearer ${token}`
+         }
+      });
+
+      if (!response.ok) {
+         throw new Error(`Error fetching the user repositories ${response.status}`);
+      }
+
+      return await response.json();
+   }
+
+}
+
+export default RepositoryService;

7ca7222585fcd4f38f84db60aeb453fee29ce365
NewName4Me <tortarod@gmail.com>
2025-04-06 17:12:25 +0200

    SSD-71 | creating 3 standar documentataion helpers, for every language

---
+++ api/src/main/java/com/api/api/repository/CodeDocumentationRepository.java
@@ -0,0 +1,17 @@
+package com.api.api.repository;
+
+import com.api.api.controller.Language;
+import com.api.api.model.CodeDocumentation;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface CodeDocumentationRepository extends MongoRepository<CodeDocumentation, String> {
+
+	List<CodeDocumentation> findByRepositoryId(String repositoryId);
+
+	List<CodeDocumentation> findByRepositoryIdAndLanguage(String repositoryId, Language language);
+
+	CodeDocumentation findByRepositoryIdAndFilePath(String repositoryId, String filePath);
+
+}

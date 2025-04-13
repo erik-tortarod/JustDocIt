7af809d521e6563a3701b1be2e7d99ecbf71889d
NewName4Me <tortarod@gmail.com>
2025-04-13 19:40:45 +0200

    CSD-79 |  creating a new route for users to access

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,15 +5,17 @@ import { Route, Routes } from "react-router-dom";
 import Auth from "./pages/Auth/Auth";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import NotFound from "./pages/NotFound/NotFound";
+import Documentation from "./pages/Documentation/Documentation";
 
 function App() {
-	return (
-		<Routes>
-			<Route path="/" element={<Auth />} />
-			<Route path="/dashboard" element={<Dashboard />} />
-			<Route path="/notfound" element={<NotFound />} />
-		</Routes>
-	);
+   return (
+      <Routes>
+         <Route path="/" element={<Auth />} />
+         <Route path="/dashboard" element={<Dashboard />} />
+         <Route path="/documentation/:id" element={<Documentation />} />
+         <Route path="/notfound/:id" element={<NotFound />} />
+      </Routes>
+   );
 }
 
 export default App;

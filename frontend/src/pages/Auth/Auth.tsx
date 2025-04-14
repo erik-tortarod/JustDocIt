99058f1cb9da11b4be87637aa0271bb557bd052b
NewName4Me <tortarod@gmail.com>
2025-04-14 14:59:45 +0200

    CSD-59 | addding the footer

--- frontend/src/pages/Auth/Auth.tsx
+++ frontend/src/pages/Auth/Auth.tsx
@@ -2,15 +2,17 @@
 import HeaderHero from "../../components/layout/HeaderHero/HeaderHero";
 import Features from "./Features";
 import Hero from "./Hero";
+import Footer from "../../components/layout/FooterHero/FooterHero";
 
 function Auth() {
-   return (
-      <div>
-         <HeaderHero />
-         <Hero />
-         <Features />
-      </div>
-   );
+	return (
+		<div>
+			<HeaderHero />
+			<Hero />
+			<Features />
+			<Footer />
+		</div>
+	);
 }
 
 export default Auth;

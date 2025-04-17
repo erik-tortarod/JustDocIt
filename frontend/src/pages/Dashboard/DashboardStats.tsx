74d606554804348ee66687f0b89e349a86e99ed4
NewName4Me <tortarod@gmail.com>
2025-04-17 12:54:52 +0200

    CSD-90 | create basic stat cards

--- frontend/src/pages/Dashboard/DashboardStats.tsx
+++ frontend/src/pages/Dashboard/DashboardStats.tsx
@@ -8,11 +8,11 @@ function DashboardStats({
 	stat: string;
 }) {
 	return (
-		<div className="border border-base-content rounded p-6 py-2 pt-0 w-full">
+		<article className="border border-base-content rounded p-6 py-2 pt-0 w-full">
 			<h2 className="text-sm mb-2 text-base-content">{title}</h2>
 			<h3 className="text-3xl font-bold mb-2">{amount}</h3>
 			<h3 className="text-sm flex items-center">{stat}</h3>
-		</div>
+		</article>
 	);
 }
 

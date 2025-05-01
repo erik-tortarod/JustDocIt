4d8b5285e5a285dfee97b9ee238cb62d577d4d3d
NewName4Me <tortarod@gmail.com>
2025-05-01 21:55:05 +0200

    CSD-97 | replace svg line case for camelcase

--- frontend/src/assets/images/svg/LanguageIcon.tsx
+++ frontend/src/assets/images/svg/LanguageIcon.tsx
@@ -1,48 +1,48 @@
 import { SVGProps } from "react";
 
 export const LanguageIcon = (props: SVGProps<SVGSVGElement>) => (
-	<svg
-		width="62"
-		height="60"
-		viewBox="0 0 62 60"
-		fill="none"
-		xmlns="http://www.w3.org/2000/svg"
-		{...props}
-	>
-		<path
-			d="M10.3334 12.5H28.4167"
-			stroke="#DFC5C5"
-			stroke-width="4"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-		/>
-		<path
-			d="M23.25 7.5V12.5C23.25 23.545 17.466 32.5 10.3334 32.5"
-			stroke="#DFC5C5"
-			stroke-width="4"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-		/>
-		<path
-			d="M12.9166 22.5C12.9166 27.86 20.5426 32.27 30.225 32.5"
-			stroke="#DFC5C5"
-			stroke-width="4"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-		/>
-		<path
-			d="M31 50L41.3333 27.5L51.6667 50"
-			stroke="#DFC5C5"
-			stroke-width="4"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-		/>
-		<path
-			d="M49.3416 45H33.325"
-			stroke="#DFC5C5"
-			stroke-width="4"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-		/>
-	</svg>
+   <svg
+      width="62"
+      height="60"
+      viewBox="0 0 62 60"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+   >
+      <path
+         d="M10.3334 12.5H28.4167"
+         stroke="#DFC5C5"
+         strokeWidth="4"
+         strokeLinecap="round"
+         strokeLinejoin="round"
+      />
+      <path
+         d="M23.25 7.5V12.5C23.25 23.545 17.466 32.5 10.3334 32.5"
+         stroke="#DFC5C5"
+         strokeWidth="4"
+         strokeLinecap="round"
+         strokeLinejoin="round"
+      />
+      <path
+         d="M12.9166 22.5C12.9166 27.86 20.5426 32.27 30.225 32.5"
+         stroke="#DFC5C5"
+         strokeWidth="4"
+         strokeLinecap="round"
+         strokeLinejoin="round"
+      />
+      <path
+         d="M31 50L41.3333 27.5L51.6667 50"
+         stroke="#DFC5C5"
+         strokeWidth="4"
+         strokeLinecap="round"
+         strokeLinejoin="round"
+      />
+      <path
+         d="M49.3416 45H33.325"
+         stroke="#DFC5C5"
+         strokeWidth="4"
+         strokeLinecap="round"
+         strokeLinejoin="round"
+      />
+   </svg>
 );

a6d672f49867b773b77a1f6d4034e14deecb0b35
NewName4Me <tortarod@gmail.com>
2025-05-01 22:00:25 +0200

    CSD-97 | prevent same key error

--- frontend/src/pages/Dashboard/AddedRepositories.tsx
+++ frontend/src/pages/Dashboard/AddedRepositories.tsx
@@ -4,17 +4,17 @@ import { IRepository } from "../../types/interfaces";
 import AddedRepositorie from "./AddedRepositorie";
 
 function AddedRepositories({
-	addedRepositories,
+   addedRepositories,
 }: {
-	addedRepositories: IRepository[];
+   addedRepositories: IRepository[];
 }) {
-	return (
-		<section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
-			{addedRepositories.map((repo) => (
-				<AddedRepositorie key={repo.id} repo={repo} />
-			))}
-		</section>
-	);
+   return (
+      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+         {addedRepositories.map((repo) => (
+            <AddedRepositorie key={repo.githubId} repo={repo} />
+         ))}
+      </section>
+   );
 }
 
 export default AddedRepositories;

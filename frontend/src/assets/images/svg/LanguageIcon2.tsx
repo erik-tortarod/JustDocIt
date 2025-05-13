d0450d86662a706b4be7cde76b8238e377ba0a6a
NewName4Me <tortarod@gmail.com>
2025-05-13 17:34:32 +0200

    CSD | create a bunch of icons to implement in the repository settings

---
+++ frontend/src/assets/images/svg/LanguageIcon2.tsx
@@ -0,0 +1,13 @@
+import { SVGProps } from "react";
+
+export const LanguageIcon2 = (props: SVGProps<SVGSVGElement>) => (
+   <svg
+      width="12"
+      height="12"
+      viewBox="0 0 16 16"
+      fill="currentColor"
+      {...props}
+   >
+      <path d="M.293 9.293a1 1 0 000 1.414l.094.083a1 1 0 001.32-.083L8 4.414l6.293 6.293a1 1 0 001.414-1.414L8.414 2a1 1 0 00-1.414 0l-7.293 7.293z" />
+   </svg>
+);
